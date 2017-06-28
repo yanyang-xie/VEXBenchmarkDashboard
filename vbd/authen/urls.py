73db3c411d7da2c4f62fb25b9be36ec7216341a4
xie yanyang <xieyanyang@xiedeMacBook-Pro.local>
2017-06-28 10:39:33 +0800

    Initial version: Finish the django project structure

---
+++ vbd/authen/urls.py
@@ -0,0 +1,13 @@
+'''
+Created on Jun 28, 2017
+
+@author: xieyanyang
+'''
+
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'vbd.views.home', name='home'),
+]
---
+++ vbd/vbd/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', 'dashboard.views.homepage', name='homepage'),
+    
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^app1/', include('authen.urls')),
+    url(r'^app2/', include('dashboard.urls')),
+]
+
