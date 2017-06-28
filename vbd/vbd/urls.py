a9b9947a2c03cfefb8b0f224c3f627c4f04d0b44
xie yanyang <xieyanyang@xiedeMacBook-Pro.local>
2017-06-28 11:06:40 +0800

    Add common static files

--- vbd/vbd/urls.py
+++ vbd/vbd/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
     url(r'^$', 'dashboard.views.homepage', name='homepage'),
+    url(r'^about$', 'dashboard.views.about', name='about'),
     
     url(r'^admin/', include(admin.site.urls)),
     url(r'^app1/', include('authen.urls')),
