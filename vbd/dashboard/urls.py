bae8560f77749725fe238627f00c731b2c7bfc62
xie yanyang <xieyanyang@xiedeMacBook-Pro.local>
2017-06-28 15:40:14 +0800

    Add nav bar

--- vbd/dashboard/urls.py
+++ vbd/dashboard/urls.py
@@ -4,10 +4,14 @@ Created on Jun 28, 2017
 @author: xieyanyang
 '''
 
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'vbd.views.home', name='home'),
+    #url(r'^$', 'dashboard.views.home', name='home'),
+    url(r'^bop$', 'dashboard.views.benchmark_operation', name='benchmark_operation'),
+    url(r'^envop$', 'dashboard.views.env_operation', name='env_operation'),
+    url(r'^config$', 'dashboard.views.env_settings', name='env_settings'),
+    
+    url(r'^result/(?P<test_type>.+)', 'dashboard.views.benchmark_result', name='benchmark_result'),
 ]
--- vbd/dashboard/views.py
+++ vbd/dashboard/views.py
@@ -6,3 +6,19 @@ def homepage(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def benchmark_operation(request):
+    context = {'active_menu':'benchmark_operation'}
+    return render(request, 'dashboard/vod.html', context)
+
+def env_operation(request):
+    context = {'active_menu':'env_operation'}
+    return render(request, 'dashboard/vod.html', context)
+
+def env_settings(request):
+    context = {'active_menu':'env_settings'}
+    return render(request, 'dashboard/vod.html', context)
+
+def benchmark_result(request, test_type):
+    context = {'active_menu':'benchmark_result'}
+    return render(request, 'dashboard/vod.html', context)
