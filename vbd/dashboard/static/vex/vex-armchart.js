1b7c2ae04a8f974fed4b362865c5b9da0f6a7e5f
xie yanyang <yanyang.xie@gmail.com>
2017-07-13 18:39:44 +0800

    Add image to show cpu usage.

--- vbd/dashboard/static/vex/vex-armchart.js
+++ vbd/dashboard/static/vex/vex-armchart.js
@@ -1,3 +1,70 @@
+//曲线图
+//chart_id: 图表的html ID. chart_data, 数据提供.
+//graph_category_field是横轴读取数据的key, graph_category_axis_title是横轴显示的title
+//graph_value_field是纵轴读取数据的key
+//graph_balloon_title是滑动时显示数据的title
+function drawLineChart(chart_id, chart_data, graph_category_field, graph_category_axis_title, graph_value_field, graph_balloon_title){
+		AmCharts.makeChart(chart_id, {
+		  "dataProvider": chart_data,
+		  "type": "serial",
+		  "theme": "light",
+		  "marginRight": 10,
+		  "marginTop": 10,
+		  "marginLeft": 20,
+		  "autoMarginOffset": 5,
+		  "valueAxes": [{
+			"id": "v1",
+			"axisAlpha": 0,
+		    "position": "left",
+		    "ignoreAxisWidth": false,
+		    "title": graph_category_axis_title
+		  }],
+		  "balloon": {
+		    "borderThickness": 1,
+		    "shadowAlpha": 0
+		  },
+		  "graphs": [{
+		    "id": "g1",
+		    "balloon": {
+		        "drop": true,
+		        "adjustBorderColor": false,
+		        "color": "#ffffff",
+		        "type": "smoothedLine"
+		      },
+		    "fillAlphas": 0.4,
+		    "bullet": "round",
+		    "bulletBorderAlpha": 1,
+		    "bulletColor": "#FFFFFF",
+		    "bulletSize": 1,
+		    "hideBulletsCount": 50,
+		    "lineThickness": 2,
+		    "title": "red line",
+		    "useLineColorForBulletBorder": true,
+		    "valueField": graph_value_field,
+		    "balloonText": "<div style='margin:1px; font-size:12px;'>" + graph_balloon_title +": <b>[[value]]</b></div>"
+		  }],
+		  "chartCursor": {
+		    "categoryBalloonDateFormat": "JJ:NN, DD MMMM",
+		    "cursorPosition": "mouse",
+		    "valueLineEnabled": true,
+		    "valueLineBalloonEnabled": true,
+		    "cursorAlpha": 0,
+		    "zoomable": false,
+		    "valueZoomable": true,
+		    "valueLineAlpha": 0.5,
+		    "selectWithoutZooming": true,
+		  },
+		  "categoryField": graph_category_field,
+		  "categoryAxis": {
+		    "minPeriod": "mm",
+		    "parseDates": true,
+		    "dashLength": 1,
+		    "minorGridEnabled": true
+		  }
+		});
+	}
+
+
 //柱状图
 //chart_id: 图表的html ID. chart_data, 数据提供.
 //graph_category_field是横轴读取数据的key, graph_category_axis_title是横轴显示的title
